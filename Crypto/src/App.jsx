974b93a10f4fb7ec5e0d519e9f096a5f8872e225
kylanalicia <aliciakimani.ky@gmail.com>
2025-05-09 16:42:24 +0300

    completed the navbar

--- Crypto/src/App.jsx
+++ Crypto/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Crypto from './pages/Crypto'
 
 const App = () => {
   return (
     <div className='app'>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/crypto/:cryptoId" element={<Crypto/>}/>
       </Routes>
       
     </div>
--- Crypto/src/pages/Home.jsx
+++ Crypto/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import CoinArea from '../components/CoinArea'
 
 const Home = () => {
   return (
     <>
     <Navbar/>
+    <CoinArea/>
     </>
   )
 }
