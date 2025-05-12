49cea5f86050e026d8a2f3a1b78408cadc5c5a24
kylanalicia <aliciakimani.ky@gmail.com>
2025-05-12 18:53:43 +0300

    Completed the footer

--- Crypto/src/pages/Crypto.jsx
+++ Crypto/src/pages/Crypto.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import CoinPage from '../components/CoinPage'
 
 const Crypto = () => {
   return (
-    <div>Crypto</div>
+    <>
+    <Navbar/>
+    <CoinPage/>
+    <Footer/>
+    </>
   )
 }
 
--- Crypto/src/pages/Home.jsx
+++ Crypto/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import CoinArea from '../components/CoinArea'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <>
     <Navbar/>
     <CoinArea/>
+    <Footer/>
     </>
   )
 }
